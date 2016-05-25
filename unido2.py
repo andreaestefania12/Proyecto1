a373b3255c59b304fc73ef9c1903f702df5b6bed
andreaestefania12 <andreaestefania12@javerianacali.edu.co>
2016-05-25 00:15:01 -0500

    Update unido2.py

--- unido2.py
+++ unido2.py
@@ -1,5 +1,5 @@
 from tkinter import *
-import random
+
 import time
 
 #Crear ventana Principal
@@ -11,3 +11,15 @@ canvas.create_image(450,300,image=fondo)
 foto = PhotoImage(file="final.gif")
 canvas.create_image(500,300,image=foto)
 canvas.pack()
+
+def quitar(ventana):
+  ventana.destroy()
+  
+def 
+
+
+#Ventana de juego 
+vent2=Toplevel(ventana)
+vent2.title("Pinball Andrea Timaran")
+canva= Canvas(vent2,width=1000,height=600)
+fondo = PhotoImage(file="1.gif")
